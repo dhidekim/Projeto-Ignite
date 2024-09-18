f755c256ed5da7e0234b5fb5e35970f7ca54ce76
Douglas <dhidekim@gmail.com>
2024-09-18 09:37:22 -0300

    CSS Modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./styles.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Douglas"
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium distinctio earum illo nam dignissimos reprehenderit temporibus voluptates consequuntur odit velit voluptatem eveniet laborum repellendus labore unde, nemo atque corrupti nulla!"
