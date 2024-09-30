3f293e61913d1258f6be01e81767564c80345fce
Douglas <dhidekim@gmail.com>
2024-09-29 22:24:44 -0300

    Os motores do React - Inserindo comentários (Programação declarativa)

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -8,7 +8,9 @@ import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post({ author, publishedAt, content }) {
-  const [comments, setComments] = useState([1, 2]);
+  const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
+
+  const [newCommentText, setNewCommentText] = useState("");
 
   const publishedDateFormatted = format(
     publishedAt,
@@ -26,8 +28,14 @@ export function Post({ author, publishedAt, content }) {
   function handleCrateNewComment(event) {
     event.preventDefault();
 
-    setComments([...comments, comments.length + 1]);
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
   }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -61,7 +69,12 @@ export function Post({ author, publishedAt, content }) {
       <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder="Deixe um comentário" />
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
 
         <footer>
           <button type="submit">Publicar</button>
@@ -70,7 +83,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment, index) => {
-          return <Comment key={index} />;
+          return <Comment key={index} content={comment} />;
         })}
       </div>
     </article>
